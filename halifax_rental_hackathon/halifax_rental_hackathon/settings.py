2fe311858b25c99a7ba218ea4deb2912226b6129
pawan706b <pawankumark54382@gmail.com>
2024-03-24 23:37:54 -0300

    adds django extension

--- halifax_rental_hackathon/halifax_rental_hackathon/settings.py
+++ halifax_rental_hackathon/halifax_rental_hackathon/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'llm_core',
-    'scrapers'
+    'scrapers',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
